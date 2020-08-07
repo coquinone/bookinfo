704897f8ac12bd106c1d6f1292bdae12be6e1c59
Tim Wang <coquinone@users.noreply.github.com>
2020-08-07 21:13:24 +1000

    Update build-services.sh

--- src/build-services.sh
+++ src/build-services.sh
@@ -37,7 +37,7 @@ pushd "$SCRIPTDIR/details"
   docker build --pull -t "${PREFIX}/examples-bookinfo-details-v1:${VERSION}" -t "${PREFIX}/examples-bookinfo-details-v1:latest" -t "${PREFIX}/examples-bookinfo-details-v1:prod" --build-arg service_version=v1 .
   #with calling external book service to fetch topic for the book
   #docker build --pull -t "${PREFIX}/examples-bookinfo-details-v2:${VERSION}" -t "${PREFIX}/examples-bookinfo-details-v2:latest" --build-arg service_version=v2 \
-	 --build-arg enable_external_book_service=true .
+#	 --build-arg enable_external_book_service=true .
 popd
 
 pushd "$SCRIPTDIR/reviews"
